7f4d6c86cd32ecf03590ec01e710e6f0e87f15d6
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-07-26 01:55:57 +0530

    DSA QUESTIONS

--- I_Multithreading/src/DSA/ReverseString.java
+++ I_Multithreading/src/DSA/ReverseString.java
@@ -1,4 +1,18 @@
 package DSA;
 
 public class ReverseString {
+    public static void main(String[] args) {
+        String name = "ADIABU";
+        System.out.println(getReverseString(name));
+
+    }
+
+
+    public static String getReverseString(String string){
+        String reversed = "";
+        for (int i = string.length()-1; i >= 0 ; i--){
+            reversed += string.charAt(i);
+        }
+        return reversed;
+    }
 }
