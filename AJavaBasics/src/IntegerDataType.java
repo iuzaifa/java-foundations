ba1adf6a86d54cb03a5a8c8f8e9beaea9b066911
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-07-31 17:11:36 +0530

    updata datatype

---
+++ AJavaBasics/src/IntegerDataType.java
@@ -0,0 +1,9 @@
+public class IntegerDataType {
+    public static void main(String[] args) {
+
+        int n = 234;
+        System.out.println(n);
+        Integer m = 234;
+        System.out.println(m);
+    }
+}
