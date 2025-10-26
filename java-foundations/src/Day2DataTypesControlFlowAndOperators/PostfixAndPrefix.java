2778ec586021a2be229e9da224899b93bcce756a
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-10-27 00:03:40 +0530

    update java code

---
+++ java-foundations/src/Day2DataTypesControlFlowAndOperators/PostfixAndPrefix.java
@@ -0,0 +1,11 @@
+package Day2DataTypesControlFlowAndOperators;
+
+public class PostfixAndPrefix {
+
+    static void main(){
+        int a = 0;
+        System.out.println(++a + ++a  + ++a);
+        //  1 + 2 + 3 = 6
+
+    }
+}
