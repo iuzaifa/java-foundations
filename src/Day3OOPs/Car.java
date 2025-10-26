2fcb5e21eecc90236e3a6e48e3d66b8759d130a8
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-10-27 00:30:56 +0530

    update java code

---
+++ src/Day3OOPs/Car.java
@@ -0,0 +1,13 @@
+package Day3OOPs;
+
+public class Car {
+
+    String color;
+    float speed;
+    String brand;
+
+
+    void drive(){
+        System.out.println(  color + " " + brand + " Rohan drive the card at speed of " + speed);
+    }
+}
---
+++ src/Day3OOPs/ClassesAndObjects.java
@@ -0,0 +1,16 @@
+package Day3OOPs;
+
+public class ClassesAndObjects {
+
+    static void main() {
+        Car car = new Car();
+        car.brand = "Toyota";
+        car.color = "red";
+        car.speed = 125;
+
+        car.drive();
+
+
+
+    }
+}
