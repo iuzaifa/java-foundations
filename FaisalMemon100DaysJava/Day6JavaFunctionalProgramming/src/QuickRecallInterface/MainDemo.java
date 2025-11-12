a2c4ea7a41b1ee377849c09ecf470af69e019b48
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-11-12 13:53:12 +0530

    Quick Recall Interface

--- FaisalMemon100DaysJava/Day6JavaFunctionalProgramming/src/QuickRecallInterface/MainDemo.java
+++ FaisalMemon100DaysJava/Day6JavaFunctionalProgramming/src/QuickRecallInterface/MainDemo.java
@@ -3,6 +3,7 @@ package QuickRecallInterface;
 public class MainDemo {
 
     public static void main(String[] args) {
+        // without Lambda expression
         Calculator calc = new Calculator() {
             @Override
             public int operate(int a, int b) {
@@ -12,6 +13,13 @@ public class MainDemo {
 
         int result = calc.operate(3,7);
         calc.printResult(result);
+        Calculator.info();
+
+
+        // with Lambda Expression
+        Calculator calculator = (a, b) -> a + b;
+        int sum = calculator.operate(33,77);
+        calculator.printResult(sum);
         Calculator.info();
     }
 }
