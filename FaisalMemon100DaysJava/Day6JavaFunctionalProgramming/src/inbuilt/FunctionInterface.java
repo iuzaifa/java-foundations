ebe1452e800fd87968debbf09840532f31e445f8
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-11-13 12:18:43 +0530

    function interface

--- FaisalMemon100DaysJava/Day6JavaFunctionalProgramming/src/inbuilt/FunctionInterface.java
+++ FaisalMemon100DaysJava/Day6JavaFunctionalProgramming/src/inbuilt/FunctionInterface.java
@@ -4,14 +4,21 @@ import java.util.function.Function;
 
 public class FunctionInterface {
 
-    static int b = 10;
-    public static Function<Integer, Integer> addFunction = (a) -> a + 34;
+    static int b = 7;
+    public static Function<Integer, Integer> addFunction = (a) -> a + 10;
     public static Function<Integer, Integer> subtract = (a) -> a - b ;
+
+    public static Function<Integer, Integer> combinedFunction = addFunction.andThen(subtract);
+
     public static Function<String, String> printName = (name)-> name;
 
     public static void main(String[] args) {
-        System.out.println("Function Interface  : "+ addFunction.apply(6));
-        System.out.println("Function Interface  : "+ subtract.apply(100));
+        System.out.println("Function Interface  : "+ addFunction.apply(10));
+        System.out.println("Function Interface  : "+ subtract.apply(20));
+
+        System.out.println("Combined Function  : "+ combinedFunction.apply(10));
+
+
         System.out.println("Function Interface print name : " + printName.apply("Abu Huzaifa"));
 
 
